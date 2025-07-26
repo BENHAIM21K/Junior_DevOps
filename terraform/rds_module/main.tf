8ef5c9ac2ef066da7d089ec990fe26859b77f25c
BENHAIM21K <ben.haim.21k@gmail.com>
2025-07-26 15:28:28 +0300

    tf changes

--- terraform/rds_module/main.tf
+++ terraform/rds_module/main.tf
@@ -1,24 +1,31 @@
-
-variable "db_name" {
-  default = "testdb123"
+data "aws_secretsmanager_secret_version" "db_credentials" {
+  secret_id = var.db_secret_name
 }
 
-variable "engine" {
-  default = "mysql"
+locals {
+  secret = jsondecode(data.aws_secretsmanager_secret_version.db_credentials.secret_string)
 }
 
-variable "env" {
-  default = "dev"
+resource "aws_db_subnet_group" "default" {
+  name       = "${var.db_name}-subnet-group"
+  subnet_ids = var.subnet_ids
+
+  tags = {
+    Name = "${var.db_name}-subnet-group"
+  }
 }
 
-variable "db_secret_name" {
-  default = "rds/credentials"
-}
-
-module "rds" {
-  source           = "./rds_module"
-  db_name          = var.db_name
-  engine           = var.engine
-  env              = var.env
-  db_secret_name   = var.db_secret_name
+resource "aws_db_instance" "this" {
+  identifier              = "${var.db_name}-db"
+  engine                  = var.engine
+  instance_class          = var.env == "prod" ? "db.t3.small" : "db.t3.micro"
+  allocated_storage       = 20
+  db_name                 = var.db_name
+  username                = local.secret.username
+  password                = local.secret.password
+  skip_final_snapshot     = true
+  db_subnet_group_name    = aws_db_subnet_group.default.name
+  vpc_security_group_ids  = []
+  publicly_accessible     = false
+  multi_az                = false
 }
--- terraform/rds_module/variables.tf
+++ terraform/rds_module/variables.tf
@@ -1,34 +1,24 @@
 variable "db_name" {
-  description = "Name of the database"
-  type        = string
+  type = string
 }
 
 variable "engine" {
-  description = "Database engine (e.g., mysql, postgres)"
-  type        = string
+  type = string
 }
 
 variable "env" {
-  description = "Environment (e.g., test, prod)"
-  type        = string
-}
-
-variable "db_secret_name" {
-  description = "Name of the secret in AWS Secrets Manager"
-  type        = string
+  type = string
 }
 
 variable "vpc_id" {
-  description = "VPC ID for RDS"
-  type        = string
+  type = string
 }
 
 variable "subnet_ids" {
-  description = "Subnet IDs for RDS subnet group"
-  type        = list(string)
+  type = list(string)
 }
 
-variable "instance_class" {
-  description = "Instance class for RDS"
-  type        = string
+variable "db_secret_name" {
+  type = string
+  default = "rds/credentials"
 }
